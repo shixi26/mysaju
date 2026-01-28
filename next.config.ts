7ff03439fc6734f71d9b3e494b1ccac9d22cb7a2
shixi26 <shixi26@gmail.com>
2026-01-28 15:40:05 +0900

    GitHub Pages 설정 수정

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,17 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // GitHub Pages 정적 배포
+  output: 'export',
+  //basePath: '/mysaju',
+  //assetPrefix: '/mysaju/',
+
+  
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: {
-    remotePatterns: [
-      {
-        hostname: '**',
-      },
-    ],
+    unoptimized: true,
   },
 };
 
